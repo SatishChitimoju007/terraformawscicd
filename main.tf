e7fed6dddb25b168570a441d0b264d29d733f8b1
SatishChitimoju007 <143459230+SatishChitimoju007@users.noreply.github.com>
2023-10-13 18:54:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "tf_Test-instance" {
   ami           = "ami-024e6efaf93d85776" # us-west-2
   instance_type = "t2.micro"
   tags = {
